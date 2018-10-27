59c66a0d386b0291504661b90c4f93cc914d7524
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-10-27 16:56:29 -0700

    Cookies
    - Cookies to preserve state in applicaiton

---
+++ src/main/java/com/dropboxlite/website/constant/CookieConst.java
@@ -0,0 +1,7 @@
+package com.dropboxlite.website.constant;
+
+public class CookieConst {
+
+  public static final String USER_ID = "userId";
+  public static final String FIRST_NAME = "firstName";
+}
