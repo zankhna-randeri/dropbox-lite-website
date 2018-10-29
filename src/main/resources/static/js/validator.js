a50a270e8f64ccaa902f2b7fa88bbf62c83e9ab1
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-10-28 19:14:39 -0700

    Password form validation

--- src/main/resources/static/js/validator.js
+++ src/main/resources/static/js/validator.js
@@ -1,8 +1,17 @@
 function validate(){
-        var size=10 * 1024 * 1024;
-        var file_size=document.getElementById('file_upload').files[0].size;
-        if(file_size>=size){
-            alert('File too large');
-            return false;
-        }
+    var size=10 * 1024 * 1024;
+    var file_size=document.getElementById('file_upload').files[0].size;
+    if(file_size>=size){
+        alert('Maximum file size allowed is 10MB');
+        return false;
+    }
+}
+
+function validateLoginform() {
+    var password = document.getElementById("inputPassword");
+    var confirm_password = document.getElementById("inputConfirmPassword");
+    if(password.value != confirm_password.value) {
+        alert("Passwords Don't Match");
+        return false;
+    }
 }
